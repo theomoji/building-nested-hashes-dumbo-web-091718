cc3f5ec72732b5d8a7591ae952b66847f1f35f29
Shuo Wang <gken90@gmail.com>
2018-09-10 20:49:54 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -16,6 +16,8 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        "Benvolio",
+        "Mercutio"
       ]
    }
 }
