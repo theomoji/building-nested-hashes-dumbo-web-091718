0b21de88baaca9bb46575eac040f4958404ba1c1
Shuo Wang <gken90@gmail.com>
2018-09-10 21:00:20 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -23,17 +23,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
-        "Steven" => {
-          :name => "Steven",
-          :age => "30",
-          :attitude => "confused"
-        }
-        "Nurse" => {
-          :name => "Nurse",
-          :age => "44",
-          :attitude => "worried"
-        }
+        {name: "Steven", age: "30", attitude: "confused"},
+        {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
 }
