956a8da0e1ba960fd6e36d60d0a571f6c363fcaf
Shuo Wang <gken90@gmail.com>
2018-09-10 20:42:24 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,6 +3,9 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
+        :name => "Lord Montague",
+        :age => "53",
+
         },
       :matriarch => {
         #your key/value pairs here
@@ -11,7 +14,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -26,5 +29,25 @@ def third_challenge
    }
   }
 
-  
+
 end
+
+
+
+The Montague :matriarch has
+a :name of "Lady Montague" and
+an :age of "54".
+The Montague :hero has
+a :name of "Romeo",
+an :age of "15", and
+a :status of "alive".
+The Capulet :patriarch has
+a :name of "Lord Capulet" and
+an :age of "50".
+The Capulet :matriarch has
+a :name of "Lady Capulet" and
+an :age of "51".
+The Capulet :heroine has
+a :name of "Juliet",
+an :age of "15", and
+a :status of "alive".
