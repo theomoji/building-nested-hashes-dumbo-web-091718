8e30b5e73876bc07cbbd215c1cca7a35a70f56ec
Shuo Wang <gken90@gmail.com>
2018-09-10 20:53:15 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -12,9 +12,9 @@ def fourth_challenge
           :attitude => "worried"
         },
         "Mercutio" => {
-          :name
-          :age
-          :attitude
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
         }
       ]
    },
