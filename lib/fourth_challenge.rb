1bf9074f53473e0b9f75ca87be3c9f948b8df849
Shuo Wang <gken90@gmail.com>
2018-09-10 20:49:40 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,8 +6,10 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
+        "Benvolio",
+        "Mercutio"
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -18,6 +20,12 @@ def fourth_challenge
    }
 }
 
-  
+
 
 end
+
+The hero's two friends are Benvolio and Mercutio. So, the :hero_friends array will contain two hashes. Each of these two hashes have the following three keys:
+
+:name
+:age
+:attitude
